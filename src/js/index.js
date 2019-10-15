45f12837c6ac32dec4b6fa5acdbbc1ba79e3f562
Tomrer <toooomra@gmail.com>
2019-10-15 16:24:09 +0300

    Deleted unnecessary files

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,1 @@
-import '../scss/main.scss';
-
-import $ from 'jquery';
-// eslint-disable-next-line no-unused-vars
-import slick from 'slick-carousel';
-window.jQuery = $;
-require("@fancyapps/fancybox");
-
-$(document).ready(function () {
-    $('.hero').slick({
-        dots: true
-    });
-});
+//import $ from 'jquery';
